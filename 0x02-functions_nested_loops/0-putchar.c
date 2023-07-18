7add388bafbb003fa4ecd64ca60cc9874e3a275e
Namuyomba-Esther <essyann11@gmail.com>
2023-07-18 01:44:55 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * 
+ * Description: 'Prints -putchar to standard output'
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	-putchar('_');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	-putchar('\n');
+	return (0);
+}
