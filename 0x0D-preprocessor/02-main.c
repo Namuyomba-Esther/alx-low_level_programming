cbe5b6ad926bc475235b7f98681b47f50ba0f468
Namuyomba-Esther <essyann11@gmail.com>
2023-08-14 17:09:05 +0000

    all

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#define NAME
 
 /**
- *  * main - prints the name of the file it was compiled from
- *   *
- *    * Return: Everything worked ok
- *     */
-
+ * main - check the code for Holberton School students.
+ * 
+ * Return: Always 0.
+ */
 int main(void)
 {
-		printf("%s\n", __BASE_FILE__);
-			return (0);
+#ifdef NAME
+	printf("%s\n", __FILE__);
+#endif
+	return (0);
 }
